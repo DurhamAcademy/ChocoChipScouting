47d51e2fd08a9e01d9c6d9908802bd410fab3093
a1cd <71281043+a1cd@users.noreply.github.com>
2023-09-24 09:59:11 -0400

    added scouting game time

--- pages/scout.vue
+++ pages/scout.vue
@@ -1,50 +1,59 @@
 <script lang="ts" setup>
-const items = [
-  [{
-    label: 'Profile',
-    avatar: {
-      src: 'https://avatars.githubusercontent.com/u/739984?v=4'
+enum GameTime {
+  Autonomous = "Auto",
+  Teleoperated = "Teleop",
+  Endgame = "Endgame"
+}
+
+
+let gameTime = ref(GameTime.Autonomous)
+
+function incrementGameTime() {
+  switch (gameTime.value) {
+    case GameTime.Autonomous:
+      gameTime.value = GameTime.Teleoperated;
+      break;
+    case GameTime.Teleoperated:
+      gameTime.value = GameTime.Endgame;
+      break
+    case GameTime.Endgame:
+      gameTime.value = GameTime.Autonomous;
+      break
     }
-  }], [{
-    label: 'Edit',
-    icon: 'i-heroicons-pencil-square-20-solid',
-    shortcuts: ['E'],
-    click: () => {
-      console.log('Edit')
-    }
-  }, {
-    label: 'Duplicate',
-    icon: 'i-heroicons-document-duplicate-20-solid',
-    shortcuts: ['D'],
-    disabled: true
-  }], [{
-    label: 'Archive',
-    icon: 'i-heroicons-archive-box-20-solid'
-  }, {
-    label: 'Move',
-    icon: 'i-heroicons-arrow-right-circle-20-solid'
-  }], [{
-    label: 'Delete',
-    icon: 'i-heroicons-trash-20-solid',
-    shortcuts: ['⌘', 'D']
-  }]
-]
+}
+
+function deincrementGameTime() {
+  switch (gameTime.value) {
+    case GameTime.Autonomous:
+      gameTime.value = GameTime.Endgame;
+      break;
+    case GameTime.Teleoperated:
+      gameTime.value = GameTime.Autonomous;
+      break;
+    case GameTime.Endgame:
+      gameTime.value = GameTime.Teleoperated;
+      break;
+  }
+}
 </script>
 
 <template>
   <Navbar scout-mode></Navbar>
-  <UContainer>
-    <UButton label="Button"/>
-    <UInput name="Input"/>
-    <UDropdown :items="items">
-      <UButton color="white" label="Options" trailing-icon="i-heroicons-chevron-down-20-solid"/>
-    </UDropdown>
-    <UCheckbox/>
-    <URange/>
-  </UContainer>
-  <UContainer>
-    <UButton label="Cancel" to="/dashboard"/>
-  </UContainer>
+  <UCard>
+    <template #header>
+      <UButtonGroup class="flex">
+        <UButton :disabled="gameTime==GameTime.Autonomous" icon="i-heroicons-chevron-left"
+                 @click="deincrementGameTime"/>
+        <UButton :label="gameTime.valueOf()" block class="w-auto" disabled style="flex-grow: 1;"/>
+        <UButton :disabled="gameTime==GameTime.Endgame" icon="i-heroicons-chevron-right" @click="incrementGameTime"/>
+      </UButtonGroup>
+    </template>
+
+    <template #footer>
+      U
+      <UButton label="Cancel" to="/dashboard" type="reset"/>
+    </template>
+  </UCard>
 </template>
 
 <style scoped>
