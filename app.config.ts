a1006d7b927c8503b24fb148af0cb3c8cbcc6d47
a1cd <71281043+a1cd@users.noreply.github.com>
2023-10-05 20:59:04 -0400

    fixed ui global options not working

---
+++ app.config.ts
@@ -0,0 +1,10 @@
+export default defineAppConfig({
+    ui: {
+        primary: "rose",
+        gray: "cool",
+        notifications: {
+            // Show toasts at the top right of the screen
+            position: 'top-0 right-0'
+        }
+    }
+});
