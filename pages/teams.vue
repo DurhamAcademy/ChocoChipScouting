f4e1fe83bf7f9547f37124c5a690a0c3ab205ba7
Ryan Bauroth <25bauroth@da.org>
2024-01-11 21:55:59 -0500

    added average amp cycles

--- pages/teams.vue
+++ pages/teams.vue
@@ -25,7 +25,7 @@ console.log(teamOrgMatches)
 let teamsData : Array<Array<any>> = []
 
 for(let [key, value] of teamOrgMatches){
-  let arr = [key, getAverageSpeakerCycles(value)]
+  let arr = [key, getAverageAmpCycles(value), getAverageSpeakerCycles(value)]
   teamsData.push(arr)
 }
 
@@ -33,6 +33,13 @@ function getAverageSpeakerCycles(teamArrays: Array<any>){
   let nonAveragedValue = 0
   for(let i = 0; i < teamArrays.length; i++){
     nonAveragedValue += teamArrays[i].auto.speakerNA + teamArrays[i].teleop.speakerNA + teamArrays[i].teleop.speakerA
+  }
+  return nonAveragedValue/teamArrays.length
+}
+function getAverageAmpCycles(teamArrays: Array<any>){
+  let nonAveragedValue = 0
+  for(let i = 0; i < teamArrays.length; i++){
+    nonAveragedValue += teamArrays[i].auto.amp + teamArrays[i].teleop.amp
   }
   return nonAveragedValue/teamArrays.length
 }
