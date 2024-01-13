3f170d410386971c704b0d0f086889833fd6b829
Ryan Bauroth <25bauroth@da.org>
2024-01-12 23:41:17 -0500

    added pie chart for auto and worked more on general teams.vue stuff :)

---
+++ components/PieChart.vue
@@ -0,0 +1,32 @@
+<template>
+  <DoughnutChart :chartData="testData" />
+</template>
+
+<script lang="ts" setup>
+import { defineComponent } from 'vue';
+import { DoughnutChart } from 'vue-chart-3';
+import { Chart, registerables } from "chart.js";
+
+const props = defineProps<{
+  labels: Array<string>,
+  data: Array<number>,
+  backgroundColors?: Array<string>
+}>()
+
+Chart.register(...registerables);
+
+let pieChartColors = ['#77CEFF', '#0079AF', '#123E6B', '#97B0C4', '#A5C8ED']
+if(props.backgroundColors)
+  pieChartColors = props.backgroundColors
+
+
+const testData = {
+  labels: props.labels,
+  datasets: [
+    {
+      data: props.data,
+      backgroundColor: pieChartColors,
+    },
+  ],
+};
+</script>
--- pages/teams.vue
+++ pages/teams.vue
@@ -23,8 +23,7 @@ for(let i  = 0; i < match.length; i++){
 let teamsData : Array<any> = []
 
 for(let [key, value] of teamOrgMatches){
-  compileEndgames(value)
-  let arr = {team: key, amp:getAverageAmpCycles(value), speaker:getAverageSpeakerCycles(value), endgame:compileEndgames(value)}
+  let arr = {team: key, amp:getAverageAmpCycles(value).toFixed(2), speaker:getAverageSpeakerCycles(value).toFixed(2), mobility:averageAuto(value).toFixed(2), endgame:compileEndgames(value)}
   teamsData.push(arr)
 }
 
@@ -44,27 +43,32 @@ function getAverageAmpCycles(teamArrays: Array<any>){
   return nonAveragedValue/teamArrays.length
 }
 
+function averageAuto(teamArrays: Array<any>){
+  let successfulMobilityCount = 0
+  for(let match of teamArrays){
+    successfulMobilityCount += match.auto.leave ? 1 : 0
+  }
+  return successfulMobilityCount/teamArrays.length
+}
+
 function compileEndgames(teamArrays: Array<any>){
   let endgameMap = new Map<string, number>();
-  for(let i = 0; i < teamArrays.length; i++){
-    let endgame = ""
-    teamArrays[i].endgame.endgame.forEach(function (value: Array<string>) {
-      endgame += endgame == "" ? value: " & " + value
+  for(let i = 0; i < teamArrays.length; i++) {
+    teamArrays[i].endgame.endgame.forEach(function (value: string) {
+        if (endgameMap.has(value)) {
+          endgameMap.set(value, endgameMap.get(value)! + 1)
+        } else
+          endgameMap.set(value, 1)
+
     })
-    if(endgameMap.has(endgame)) {
-      endgameMap.set(endgame, endgameMap.get(endgame)! + 1)
-    }
-    else
-        endgameMap.set(endgame, 1)
   }
-  let total = 0
-  for(let [, value] of endgameMap){
-    total += value
-  }
+  let endgameOptionsArr : Array<string> = []
+  let endgameDataArr : Array<number> = []
   endgameMap.forEach(function(value, key){
-    endgameMap.set(key, value/total)
+    endgameOptionsArr.push(key)
+    endgameDataArr.push(value)
   })
-  return endgameMap
+  return [endgameOptionsArr, endgameDataArr]
 }
 
 const columns = [{
@@ -77,7 +81,11 @@ const columns = [{
   key: 'speaker',
   label: 'Average Speaker Cycles'
 }, {
-  key: 'actions'
+  key: 'mobility',
+  label: 'Mobility Success Rate'
+}, {
+  key: 'actions',
+  label: 'Endgame'
 }]
 
 
@@ -86,9 +94,33 @@ const columns = [{
 <template>
 <Navbar></Navbar>
   <UTable :rows="teamsData" :columns="columns">
-    <template #actions-data="{ row }">
-      <p>{{row.endgame}}</p>
+
+    <template #amp-data="{ row }">
+      <UPopover>
+        <UButton class="m-1" color="white" variant="soft" >{{row.amp}}</UButton>
+        <template #panel>
+          <UCard>
+            <div class="max-w-xs min-w-[15rem] overflow-y-auto" style="max-height: 20rem; min-height: 10rem">
+
+            </div>
+          </UCard>
+        </template>
+      </UPopover>
     </template>
+
+    <template #actions-data="{ row }">
+      <UPopover>
+        <UButton class="m-1" color="blue" label="Chart" variant="soft" />
+        <template #panel>
+          <UCard>
+            <div class="max-w-xs min-w-[15rem] overflow-y-auto" style="max-height: 20rem; min-height: 10rem">
+              <PieChart :labels="row.endgame[0]" :data="row.endgame[1]"/>
+            </div>
+          </UCard>
+        </template>
+      </UPopover>
+    </template>
+
   </UTable>
 </template>
 
