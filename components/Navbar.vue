33a34fc35718c2b0354a942937ccb4edde3905da
Ryan Bauroth <25bauroth@da.org>
2024-02-02 21:01:54 -0500

    made significant changes to OuterComponents.vue and Navbar.vue to remove profile picture and top bar and add a settings icon. Still needs work to make side bar not scroll with page

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -14,6 +14,10 @@ const {usernameState, sessionState, logout}: {
   updateUsernameState: () => Promise<boolean>
 } = inject(loginStateKey)!
 
+const events = ['2024test', '2024trial']
+
+let selectedEvent = ref(window.localStorage.getItem("event"))
+
 let props = defineProps({
   scoutMode: {
     type: Boolean,
@@ -33,7 +37,6 @@ let links: VerticalNavigationLink[] = [
   { label: "Dashboard", to: "/dashboard" },
   { label: "Matches", to: "/matches" },
   { label: "Teams", to: "/teams" },
-  { label: "Competitions", to: "/competitions" },
   { label: "Attachments", to: "/attachments" },
   { label: "Contacts", to: "/contacts" }
 ]
@@ -41,47 +44,40 @@ console.log(sessionState?.value?.userCtx?.roles)
 if (sessionState?.value?.userCtx?.roles?.indexOf('_admin') != -1)
   links.push({ label: "Users", to: "/users" })
 
-const events = ['2024test', '2024trial']
-
-let selectedEvent = ref(window.localStorage.getItem("event"))
-
-function updateEvent(value: string){
-  window.localStorage.setItem("event", value)
-}
 </script>
 
 <template>
-  <div class="py-2.5 px-5 z-10 bg-opacity-50 bg-gray-200 dark:bg-gray-700 w-screen navbar-right sticky top-0 flex-row justify-between backdrop-blur-lg">
-    <div class="flex">
+  <div v-if="!disableSidebar" class="py-2.5 px-5 z-10 bg-opacity-50 bg-gray-200 dark:bg-gray-700 w-screen navbar-right sticky top-0 flex-row justify-between backdrop-blur-lg min-h-16">
       <Transition name="slide-fade">
-        <UButton class="slide-nothing" v-if="!disableSidebar" icon="i-heroicons-bars-3-20-solid" variant="ghost" @click="isOpen=!isOpen" :size="scoutMode?'sm':'xl'" square/>
+          <UButton class="slide-nothing" v-if="!disableSidebar" icon="i-heroicons-bars-3-20-solid" variant="ghost" @click="isOpen=!isOpen" :size="'xl'" square style="position:absolute; top:50%; transform: translateY(-50%)"/>
       </Transition>
-      <UPopover style="right: 1%; position: absolute;top: 50%;transform: translateY(-50%);">
-        <UButton variant="ghost">
-          <UAvatar v-if="usernameState!==undefined" :alt="usernameState" :size="scoutMode?'xs':'md'" class="m-1"/>
-        </UButton>
-        <template #panel>
-          <UCard class="p-2">
-            <template #header>
-              {{usernameState}}
-            </template>
-            <UFormGroup class="inputDiv" label="Event" name="event">
-              <USelectMenu v-model="selectedEvent" :options="events" @update:model-value ="value => {updateEvent(value)}"/>
-            </UFormGroup>
-            <template #footer>
-              <UButton block label="Logout" square @click="logout"/>
-            </template>
-          </UCard>
-        </template>
-      </UPopover>
-    </div>
   </div>
   <USlideover v-model="isOpen" side="left" :appear="!disableSidebar">
     <UCard class="flex flex-col h-screen">
       <template #header>
-        <UButton icon="i-heroicons-bars-3-20-solid" variant="ghost" @click="isOpen=!isOpen" :size="scoutMode?'sm':'xl'"/>
+        <UButton icon="i-heroicons-bars-3-20-solid" variant="ghost" @click="isOpen=!isOpen" :size="'xl'"/>
       </template>
       <UVerticalNavigation :links="links"/>
+      <div style="position:absolute; left:4%; bottom: 2%">
+        <UPopover>
+          <UButton icon="i-heroicons-cog-6-tooth" square :size="'xl'" :variant="'ghost'" :color="'gray'"/>
+          <template #panel>
+            <UCard class="p-2">
+              <template #header>
+                <div class="max-w-32" style="color:black; overflow:hidden">
+                  {{usernameState}}
+                </div>
+              </template>
+              <UFormGroup class="inputDiv" label="Event" name="event">
+                <USelectMenu v-model="selectedEvent" :options="events" @update:model-value ="value => {updateEvent(value)}"/>
+              </UFormGroup>
+              <template #footer>
+                <UButton block label="Logout" square @click="logout"/>
+              </template>
+            </UCard>
+          </template>
+        </UPopover>
+      </div>
     </UCard>
   </USlideover>
 </template>
--- components/OuterComponents.vue
+++ components/OuterComponents.vue
@@ -10,19 +10,37 @@
   import {couchDBBaseURL} from "~/utils/URIs";
 
   const usersDB = new PouchDB(`${couchDBBaseURL}/_users`, {skip_setup: true});
+  const session = await usersDB.getSession()
 
   let {width, height} = useWindowSize()
 
   let route = useRoute()
 
+  const events = ['2024test', '2024trial']
+
+  let selectedEvent = ref(window.localStorage.getItem("event"))
+
+  function updateEvent(value: string){
+    window.localStorage.setItem("event", value)
+  }
+
+  const {usernameState, sessionState, logout}: {
+    logout: () => Promise<void>;
+    // noinspection TypeScriptUnresolvedReference
+    loginState: Ref<UnwrapRef<LoginState>>;
+    // noinspection TypeScriptUnresolvedReference
+    sessionState: Ref<UnwrapRef<PouchDB.Authentication.SessionResponse>>;
+    // noinspection TypeScriptUnresolvedReference
+    usernameState: Ref<UnwrapRef<string>>;
+    updateUsernameState: () => Promise<boolean>
+  } = inject(loginStateKey)!
+
   let links: VerticalNavigationLink[] = [
     { label: "Dashboard", to: "/dashboard" },
     { label: "Matches", to: "/matches" },
     { label: "Teams", to: "/teams" },
-    { label: "Competitions", to: "/competitions" },
     { label: "Attachments", to: "/attachments" }
   ]
-  let session = await usersDB.getSession()
   if (session.userCtx.roles?.indexOf("_admin") != -1){
     links.push({ label: "Users", to: "/users" })
   }
@@ -34,9 +52,29 @@
     <Navbar class="flex-grow basis-auto" :disable-sidebar="width > 800"/>
     <div class="flex flex-grow flex-shrink basis-auto flex-row w-screen overflow-x-clip max-h-none place-content-around min-h-full">
       <Transition name="width-fade">
-        <div v-show="width > 800" class="vis min-h-full max-w-full">
+        <div v-show="width > 800" class="vis min-h-screen h-screen max-w-full">
           <UCard class="h-full" :ui="{rounded: 'rounded-none'}">
             <UVerticalNavigation :links="links"/>
+            <div style="position:absolute; left:5.7%; bottom: 2.15%">
+              <UPopover>
+              <UButton icon="i-heroicons-cog-6-tooth" square :size="'xl'" :variant="'ghost'" :color="'gray'"/>
+                  <template #panel>
+                    <UCard class="p-2">
+                      <template #header>
+                        <div class="max-w-32" style="color:black; overflow:hidden">
+                          {{usernameState}}
+                        </div>
+                      </template>
+                        <UFormGroup class="inputDiv" label="Event" name="event">
+                          <USelectMenu v-model="selectedEvent" :options="events" @update:model-value ="value => {updateEvent(value)}"/>
+                        </UFormGroup>
+                      <template #footer>
+                        <UButton block label="Logout" square @click="logout"/>
+                      </template>
+                    </UCard>
+                  </template>
+              </UPopover>
+            </div>
           </UCard>
         </div>
       </Transition>
