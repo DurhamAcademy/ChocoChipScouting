0033b61824d483371855ff299dfec6784b718833
Ryan Bauroth <25bauroth@da.org>
2024-02-03 15:38:00 -0500

    created auto flex divs for each alliance

--- pages/coach-preview.vue
+++ pages/coach-preview.vue
@@ -1,10 +1,30 @@
 <script setup lang="ts">
 
+  let teams = ref([[6502, 700, 12], [7897, 3423, 32]])
+  const colorAssignments = ["red", "blue"]
+
+  let friendlyAlliance = true
+  let opposingAlliance = true
+
+  function test(){
+
+  }
 </script>
 
 <template>
   <OuterComponents>
-
+  <div class="text-center w-full h-full flex flex-col">
+      <div v-if="friendlyAlliance" class="flex flex-auto">
+        <div v-for="(team) in teams[0]" class="text-blue-500 flex-auto">
+          {{team}}
+        </div>
+      </div>
+    <div v-if="opposingAlliance" class="flex flex-auto">
+      <div v-for="(team) in teams[1]" class="text-red-500 flex-auto">
+        {{team}}
+      </div>
+    </div>
+  </div>
   </OuterComponents>
 </template>
 
