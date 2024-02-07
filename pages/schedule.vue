2e1beff4127b7ad0a6e22f66bc6587b403a34b55
alexru26 <rualex2008@gmail.com>
2024-02-07 16:23:34 -0500

    Add notes for Justin :)
    Signed-off-by: alexru26 <rualex2008@gmail.com>

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -18,16 +18,20 @@ async function dataPull(entry: any): Promise<any>{
   let grab: any;
   grab = await fetch(urlFinal, {
     method: 'GET',
-    headers: {
+    headers: { // TODO: eventually conceal this by doing it on server side
       'X-TBA-Auth-Key': "JBP0wpGwe79xWOVzDXWFKxgmFhZEmrIgVluq3PZf4z9OVcvROKTjnTrRu7D9rsUz"
     }
   });
   grab = await grab.json();
   let teamList: any = []
+  // TODO: @26spitzer maybe do some mapping? also you can use .team_number instead of .key
   for (let i in grab){
+    /*
     let withFRC: any = grab[i].key;
     let unFRC: any = withFRC.slice(3)
     teamList.push(unFRC)
+     */
+    teamList.push(grab[i].team_number)
   }
   console.log(teamList.toString())
   return;
