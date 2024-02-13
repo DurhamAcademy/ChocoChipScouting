53fde8e473b2a1c9ee78ae13c535a902c2cbb71a
Ryan Bauroth <25bauroth@da.org>
2024-02-13 16:14:50 -0500

    test

--- pages/login.vue
+++ pages/login.vue
@@ -3,10 +3,6 @@ import "../utils/authorization/Authorizer";
 import {couchDBBaseURL} from "~/utils/URIs"
 import {loginStateKey} from "~/utils/keys";
 
-/*
-test test test
- */
-
 const usersDB = new PouchDB(`${couchDBBaseURL}/_users`, {skip_setup: true});
   let username = ref("");
   let password = ref("");
