9e27c5dd3bae2c5ed7afb673265167a2fbe63fc5
Ryan Bauroth <25bauroth@da.org>
2024-02-16 15:38:12 -0500

    fixed bug got rid of tba key leak

--- server/api/eventMatches/[event].ts
+++ server/api/eventMatches/[event].ts
@@ -1,23 +1,3 @@
-//const TBA_KEY = process.env.TBA_KEY
-//this is alr leaked so i dont feel bad :shrug:
-const TBA_KEY = 'JBP0wpGwe79xWOVzDXWFKxgmFhZEmrIgVluq3PZf4z9OVcvROKTjnTrRu7D9rsUz'
-
-/*export async function getEventMatches(event: string) {
-    console.log(useRuntimeConfig().TBA_Key)
-    if (TBA_KEY != undefined) {
-        let urlNoNum: string = "https://www.thebluealliance.com/api/v3/";
-        let urlFinal: string = urlNoNum + "event/" + event + "/matches";
-        let grab: any;
-        grab = await fetch(urlFinal, {
-            method: 'GET',
-            headers: {
-                'X-TBA-Auth-Key': TBA_KEY
-            }
-        });
-        return await grab.json();
-    }
-}*/
-
 export default defineEventHandler(async (event) => {
     const eventKey = getRouterParam(event, 'event')
     let config = useRuntimeConfig()
@@ -28,7 +8,7 @@ export default defineEventHandler(async (event) => {
         grab = await fetch(urlFinal, {
             method: 'GET',
             headers: {
-                'X-TBA-Auth-Key': TBA_KEY
+                'X-TBA-Auth-Key': config.TBA_Key
             }
         });
          return await grab.json();
