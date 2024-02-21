958de70c3fe95437dff9440e6ffdcf2521c3967c
RagerGitHub <103478729+RagerGitHub@users.noreply.github.com>
2024-02-21 11:17:20 -0500

    organized variables a little bit added filters feature for tags and text input clicking on attachment opens carousel to that specific attachment by moving it to the first index of the carousel tweaked look of carousel added a v-else for when there are no attachments that match the filter or there are no attachments for specified team

--- pages/teams/[id].vue
+++ pages/teams/[id].vue
@@ -5,46 +5,76 @@ import databases from "~/utils/databases"
 
 const { attachments } = databases.locals
 const db = attachments
-const attachmentsData = ref<({ attachmentURL: string; attachmentID: string; tagList: string[]; notes: string; fileName: string; fileSize: string; event: string; author: string | undefined; dateUploaded: string})[]>([])
-let attachmentsURLs = ref<string[]>([])
 const route = useRoute()
-const attachmentHovered = ref(attachmentsData.value.map(() => false))
+
+// display stuff
+const attachmentsData = ref<({ attachmentURL: string; attachmentID: string; tagList: string[]; notes: string; fileName: string; fileSize: string; event: string; author: string | undefined; dateUploaded: string; attachmentHovered: boolean})[]>([])
+const filteredAttachmentsData = ref<({ attachmentURL: string; attachmentID: string; tagList: string[]; notes: string; fileName: string; fileSize: string; event: string; author: string | undefined; dateUploaded: string; attachmentHovered: boolean})[]>([])
+const tempCarouselData = ref<({ attachmentURL: string; attachmentID: string; tagList: string[]; notes: string; fileName: string; fileSize: string; event: string; author: string | undefined; dateUploaded: string; attachmentHovered: boolean})[]>([])
+const attachmentsURLs = ref<string[]>([])
+const displayURLs = ref<string[]>([])
+const possibleTags = ref<string[]>([])
+const tagStyles = ref<string[]>([])
+const attachmentHovered = ref(attachmentsData.value.map(() => false)) // creates a list of bools for each of the attachments (all set to false because none are being hovered)
 const openCarousel = ref(false)
 
-let carouselImages = []
+// filters
+const filterTags = ref<string[]>([])
+const filterInput = ref<string>('')
 
-let alldocs = await db.allDocs({ include_docs: true })
-  // Filter attachments by key
-  alldocs.rows.forEach(async (row) => {
-    const doc = row.doc;
-    if (doc)
-      if (doc._attachments && doc.teamNumber.toString() == route.params.id) {
-        // Process the attachments for this document
-        let attachment = await (db.getAttachment(doc._id, doc.name))
-        if (attachment instanceof Blob) {
-          let file = new File([attachment], doc.name, {type: attachment.type})
+let allDocs = await db.allDocs({ include_docs: true })
+// filter attachments by team #
+allDocs.rows.forEach(async (row) => {
+  const doc = row.doc;
+  if (doc)
+    if (doc._attachments && doc.teamNumber.toString() == route.params.id) {
+      // Process the attachments for this document
+      let attachment = await (db.getAttachment(doc._id, doc.name))
+      if (attachment instanceof Blob) {
+        let file = new File([attachment], doc.name, {type: attachment.type})
 
-          attachmentsData.value.push({ attachmentURL: URL.createObjectURL(file), attachmentID: doc._id, tagList: doc.tags, notes: doc.extraNotes, fileName: doc.name, fileSize: doc.fileSize, event: doc.event, author: doc.author, dateUploaded: doc.dateUploaded})
+        attachmentsData.value.push({ attachmentURL: URL.createObjectURL(file), attachmentID: doc._id, tagList: doc.tags, notes: doc.extraNotes, fileName: doc.name, fileSize: doc.fileSize, event: doc.event, author: doc.author, dateUploaded: doc.dateUploaded, attachmentHovered: false})
+        for (let tag of attachmentsData.value[attachmentsData.value.length-1].tagList) {
+          if(!possibleTags.value.includes(tag)) {
+            possibleTags.value.push(tag)
+            tagStyles.value.push("outline")
+          }
         }
-
       }
-  });
+
+    }
+});
+filteredAttachmentsData.value = attachmentsData.value
+
+// updating stuff when values change
 watchEffect(() => {
   attachmentsURLs.value = attachmentsData.value.map(item => item.attachmentURL);
+  filteredAttachmentsData.value = attachmentsData.value.filter(attachment => {
+    return attachment.notes.toLowerCase().includes(filterInput.value.toLowerCase());
+  }).filter(attachment => {
+    return filterTags.value.every(tag => attachment.tagList.includes(tag))
+  })
 })
-console.log(attachmentsURLs)
 
-function updateAttachmentNote(index: number, note: string) {
-  if (attachmentsData.value[index].notes === "")
-    attachmentsData.value[index].notes = "No Notes"
-  else
-    attachmentsData.value[index].notes = note
+// changes tag variant of tag clicked and also adds/removes tag to filter tags
+function toggleTag(index: number) {
+  if (tagStyles.value[index] == "outline") {
+    filterTags.value.push(possibleTags.value[index])
+    tagStyles.value[index] = "solid"
+  } else if (tagStyles.value[index] == "solid"){
+    filterTags.value.splice(filterTags.value.indexOf(possibleTags.value[index]))
+    tagStyles.value[index] = "outline"
+  }
 }
 
-let team = "Team " + route.params.id + " Attachments"
-
-function showCarousel() {
-  carouselImages = attachmentsURLs.value
+// sets image clicked on as the first image in the carousel and makes sure that carousel data is parallel to displayURLs array
+function showCarousel(index: number) {
+  displayURLs.value = [...attachmentsURLs.value]
+  tempCarouselData.value = [...attachmentsData.value]
+  const temp = displayURLs.value.splice(index, 1)[0]
+  const temp2 = tempCarouselData.value.splice(index, 1)[0]
+  displayURLs.value.unshift(temp)
+  tempCarouselData.value.unshift(temp2)
   openCarousel.value = true
 }
 
@@ -58,11 +88,18 @@ function showCarousel() {
       <div>
         <h1 class="font-extrabold text-4xl">Team {{ route.params.id }} Attachments</h1>
         <div class="flex mt-2">
-          <UInput placeholder="Filter Notes" icon="i-heroicons-magnifying-glass" class="w-40"/> <!-- wip -->
+          <UInput placeholder="Filter Notes" icon="i-heroicons-magnifying-glass" class="w-40" v-model="filterInput"/> <!-- wip -->
           <UPopover class="px-2">
-            <UButton label="Filter Tags" trailing-icon="i-heroicons-adjustments-horizontal" variant="ghost"/> <!-- also wip-->
+            <UButton label="Filter Tags" trailing-icon="i-heroicons-adjustments-horizontal" variant="ghost"/>
             <template #panel>
-
+              <UCard class="min-w-32 max-w-64 flex flex-wrap justify-center">
+                <template #header>
+                  <h1>Choose Tags To Filter</h1>
+                </template>
+                <template #default>
+                  <UButton v-for="(tag, index) in possibleTags" :label="tag" class="flex-grow justify center" style="margin:5px" :variant="tagStyles[index]" :ui="{ rounded: 'rounded-full' }" @click="toggleTag(index)"/>
+                </template>
+              </UCard>
             </template>
           </UPopover>
         </div>
@@ -70,10 +107,10 @@ function showCarousel() {
     </template>
     <template class="flex flex-wrap justify-center">
       <UModal v-model="openCarousel" fullscreen >
-        <UButton color="gray" icon="i-heroicons-x-mark" size="xl" class="absolute right-2 top-2" variant="ghost" @click="openCarousel=false"/>
+        <UButton color="gray" icon="i-heroicons-x-mark" size="xl" class="absolute right-2 top-2" variant="ghost" @click="openCarousel=false "/>
           <UCarousel
               v-slot="{ item, index }"
-              :items="attachmentsURLs"
+              :items="displayURLs"
               :ui="{
         item: 'basis-full justify-center',
         container: 'rounded-lg bg-gray-100'
@@ -82,45 +119,46 @@ function showCarousel() {
               :prev-button="{
         color: 'gray',
         icon: 'i-heroicons-arrow-left-20-solid',
-        class: '-left-12'
+        class: 'left-6'
       }"
               :next-button="{
         color: 'gray',
         icon: 'i-heroicons-arrow-right-20-solid',
-        class: '-right-12'
+        class: 'right-6'
       }"
               arrows
-              class="w-11/12 h-11/12 m-auto"
+              class="w-full h-11/12 m-auto"
           >
-              <NuxtImg :src="item" draggable="false" class="object-contain"/>
-              <UCard class="bg-white rounded-2xl m-2 min-w-64 ">
-                <template #header class="flex flex-wrap">
-                  <div class="font-sans text-2xl flex flex-wrap" >
-                    <h1 v-if="attachmentsData[index].notes != ''" >{{ attachmentsData[index].notes }}</h1> <!-- push contents to the database -->
-                    <h1 class="opacity-50" v-else>No Notes</h1> <!--fix this horrendous pile of shit-->
-                  </div>
-                </template>
-                <template class="flex flex-wrap">
-                  <UBadge v-if="attachmentsData[index].tagList.length != 0" v-for="tag in attachmentsData[index].tagList" :label="tag" class="bg-gray-500 m-0.5 rounded-2xl h-7 px-3" variant="solid"/>
-                  <h1 v-else class="font-sans text-lg opacity-50">No Tags</h1>
-                </template>
-                <template #footer>
-                  <div class="font-sans text-xs opacity-35">
-                    <p>Event: {{attachmentsData[index].event}}</p>
-                    <p>Author: {{attachmentsData[index].author}}</p>
-                    <p>Date Uploaded: {{attachmentsData[index].dateUploaded}}</p>
-                  </div>
-                </template>
-              </UCard>
+
+            <NuxtImg :src="item" draggable="false" class="object-contain" />
+            <UCard class="bg-white rounded-2xl m-2 lg:min-w-64 md:min-w-48 sm:min-w-24">
+              <template #header class="flex flex-wrap">
+                <div class="font-sans text-2xl flex flex-wrap" >
+                  <h1 v-if="tempCarouselData[index].notes != ''" >{{ tempCarouselData[index].notes }}</h1>
+                  <h1 v-else class="opacity-50">No Notes</h1>
+                </div>
+              </template>
+              <template class="flex flex-wrap">
+                <UBadge v-if="tempCarouselData[index].tagList.length != 0" v-for="tag in tempCarouselData[index].tagList" :label="tag" class="bg-gray-500 m-0.5 rounded-2xl h-7 px-3" variant="solid"/>
+                <h1 v-else class="font-sans text-lg opacity-50">No Tags</h1>
+              </template>
+              <template #footer>
+                <div class="font-sans text-xs opacity-35">
+                  <p>Event: {{tempCarouselData[index].event}}</p>
+                  <p>Author: {{tempCarouselData[index].author}}</p>
+                  <p>Date Uploaded: {{tempCarouselData[index].dateUploaded}}</p>
+                </div>
+              </template>
+            </UCard>
           </UCarousel>
       </UModal>
-      <UContainer v-for="(attachment, index) in attachmentsData" :key="index"  class="m-2 bg-cover bg-center min-w-60 w-1/4 h-80 rounded-2xl md:px-0 lg:px-0 sm:px-0 px-0 flex flex-col justify-end" :style="{ backgroundImage: 'url(' + attachment.attachmentURL + ')'}" @mouseover="attachmentHovered[index] = true" @mouseleave="attachmentHovered[index] = false" @click="showCarousel">
+      <UContainer v-if="filteredAttachmentsData.length != 0" v-for="(attachment, index) in filteredAttachmentsData" :key="index"  class="m-2 bg-cover bg-center min-w-60 w-1/4 h-80 rounded-2xl md:px-0 lg:px-0 sm:px-0 px-0 flex flex-col justify-end" :style="{ backgroundImage: 'url(' + attachment.attachmentURL + ')'}" @mouseover="attachmentHovered[index] = true" @mouseleave="attachmentHovered[index] = false" @click="showCarousel(index)">
           <UContainer v-show="attachmentHovered[index]" class="lg:px-0 sm:px-0 px-0 m-0 h-30 max-w-7xl ">
             <UCard class="rounded-t-none rounded-b-2xl bg-white" :style="{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }">
                 <div class="flex flex-wrap">
                   <div class="font-sans text-2xl flex flex-wrap" >
-                    <h1 v-if="attachment.notes != ''" >{{ attachment.notes }}</h1> <!-- push contents to the database -->
-                    <h1 class="opacity-50" v-else>No Notes</h1> <!--fix this horrendous pile of shit-->
+                    <h1 v-if="attachment.notes != ''" >{{ attachment.notes }}</h1>
+                    <h1 class="opacity-50" v-else>No Notes</h1>
                   </div>
 
                   <!--
@@ -146,6 +184,7 @@ function showCarousel() {
             </UCard>
           </UContainer>
       </UContainer>
+      <p v-else class="font-sans text-lg font-bold opacity-70">Placeholder for sad not found cookie image or something</p>
     </template>
   </UCard>
 </template>
