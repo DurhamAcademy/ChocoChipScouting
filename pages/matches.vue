d555fee930a3ce85455c52b6e51c4e461fde7412
Ryan Bauroth <25bauroth@da.org>
2024-02-21 11:26:11 -0500

    testing event issues

--- pages/matches.vue
+++ pages/matches.vue
@@ -9,6 +9,8 @@ const currentEvent = localStorage.getItem('currentEvent') || eventOptions[0]
 
 let db = scoutingData
 
+let test = ref("")
+
 const matche = (await db.allDocs()).rows
 let matc = matche.map(async (doc) => {
   return await db.get(doc.id)
@@ -16,8 +18,8 @@ let matc = matche.map(async (doc) => {
 let matches = await Promise.all(matc)
 for (let i=matches.length-1; i>=0; i--) {
   if(matches[i].matchNumber === -1 || matches[i].matchNumber === null || (matches[i].event != currentEvent)) matches.splice(i, 1)
+  test.value = matches[i].event
 }
-
 const headers = [
   {
     title: 'Info',
@@ -81,6 +83,7 @@ const items = matches
         </UPopover>
       </template>
     </VDataTable>
+    <p>{{test}}</p>
   </OuterComponents>
 </template>
 <style scoped>
