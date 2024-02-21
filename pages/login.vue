18a06ae5edf75d241f07f3160f35d3a9ee472c4f
a1cd <71281043+a1cd@users.noreply.github.com>
2024-02-21 09:32:47 -0500

    added more auth plugin stuff
    Signed-off-by: a1cd <71281043+a1cd@users.noreply.github.com>

--- pages/login.vue
+++ pages/login.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import "../utils/authorization/Authorizer";
+import PouchDB from "pouchdb"
+import auth from "../utils/authorization/Authorizer";
 import {couchDBBaseURL} from "~/utils/URIs"
 import {loginStateKey} from "~/utils/keys";
 import {eventOptions} from "~/utils/eventOptions";
-import PouchDB from "pouchdb"
 
+PouchDB.plugin(auth)
 const usersDB = new PouchDB(`${couchDBBaseURL}/basic`, {skip_setup: true});
   let username = ref("");
   let password = ref("");
