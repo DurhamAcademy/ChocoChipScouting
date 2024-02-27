391cd88037f3df4d4e2fec72d3d1fc367ebc4d1e
a1cd <71281043+a1cd@users.noreply.github.com>
2024-02-27 10:53:52 -0500

    made couchdbBaseUrl adapt to a secure context
    Signed-off-by: a1cd <71281043+a1cd@users.noreply.github.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,6 +25,7 @@ export default defineNuxtConfig({
       brotli: false,
       gzip: true
     },
+
   },
   app: {
     head: {
--- utils/URIs.ts
+++ utils/URIs.ts
@@ -1,5 +1,20 @@
 
-const couchDBBaseURL = (typeof window !== 'undefined')?`http://${window.location.hostname}:5984`:(process.env.couchDBHostname!==undefined)?`http://${process.env.couchDBHostname}:5984`:`http://localhost:5984`
+let couchDBBaseURL: string;
+if (typeof window !== 'undefined') {
+    let protocol = "http://"
+    let port = 5984
+    if (window.isSecureContext) {
+        port = 6984
+        protocol = "https://"
+    }
+    couchDBBaseURL = `${protocol}${window.location.hostname}:5984`;
+} else {
+    if (process.env.couchDBHostname !== undefined) {
+        couchDBBaseURL = `http://${process.env.couchDBHostname}:5984`;
+    } else {
+        couchDBBaseURL = `http://localhost:5984`;
+    }
+}
 
 export {
     couchDBBaseURL
