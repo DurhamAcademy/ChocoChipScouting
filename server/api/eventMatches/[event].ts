46a89b98888ee6cd5ee7c2f3102bf7b792f24e20
Ryan Bauroth <25bauroth@da.org>
2024-02-28 15:53:22 -0500

    server testing

--- server/api/eventMatches/[event].ts
+++ server/api/eventMatches/[event].ts
@@ -1,17 +1,17 @@
 export default defineEventHandler(async (event) => {
     const eventKey = getRouterParam(event, 'event')
     let config = useRuntimeConfig();
-    if (config.TBA_KEY != undefined) {
+    if (config.tbaKey != undefined) {
         let urlNoNum: string = "https://www.thebluealliance.com/api/v3/";
         let urlFinal: string = urlNoNum + "event/" + eventKey + "/matches";
         let grab: any;
         grab = await fetch(urlFinal, {
             method: 'GET',
             headers: {
-                'X-TBA-Auth-Key': config.TBA_KEY
+                'X-TBA-Auth-Key': config.tbaKey
             }
         });
-        return config.TBA_KEY
+        return config.tbaKey
         return await grab.json();
     }
     else{
