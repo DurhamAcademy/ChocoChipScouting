190196b6c40cdeb7e9eee70f1ca9c9ed575a99a3
Preston Swigart <prestonlswigart@gmail.com>
2024-03-14 23:58:31 -0400

    work in progress

--- components/AutoRoute.vue
+++ components/AutoRoute.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
-const emit = defineEmits(['update:result'])
+import { computed } from 'vue'
+const emit = defineEmits(['update:modelValue'])
 const props = defineProps<{
+  modelValue: string[]
 }>()
 let
     index: string,
-    fullArray: [["f1", "f2", "f3", "f4"],["m1", "m2", "m3", "m4"],["c1", "c2", "c3", "c4"],["s1", "s2", "s3", "s4"]],
+    fullArray: [["f1", "f2", "f3", "f4"],["m1", "m2", "m3", "m4"],["c1", "c2", "c3", "c4"],["s1", "s2", "s3", "s4"]]
     //f = front m = middle c = climb s = speaker
-    result: string[]
+
+const value = computed({
+  get() {
+    return props.modelValue
+  },
+  set(value) {
+    emit('update:modelValue', value)
+  }
+})
 </script>
 
 <template>
