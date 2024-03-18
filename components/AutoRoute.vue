723c73eedc50ce70f78ba986a44d3ecd8553dc7c
Preston Swigart <prestonlswigart@gmail.com>
2024-03-18 14:54:12 -0400

    work in progress

--- components/AutoRoute.vue
+++ components/AutoRoute.vue
@@ -6,6 +6,7 @@ const props = defineProps<{
 }>()
 let fullArray = [["f1", "f2", "f3", "f4"],["m1", "m2", "m3", "m4"],["c1", "c2", "c3", "c4"],["s1", "s2", "s3", "s4"]]
     //f = front m = middle c = climb s = speaker
+let isOpen = ref(false)
 
 const value = computed({
   get() {
@@ -16,19 +17,26 @@ const value = computed({
   }
 })
 
-function whenClicked(Quadrant:string){
-  console.log(Quadrant)
-}
 
 </script>
 
 <template>
   <div>
     <div v-for="items in fullArray" class="flex" flex-auto>
-      <div v-for="item in items" @click="whenClicked(item)" class="flex-auto w-1/4 h-1/4">
+      <div v-for="item in items" @click="isOpen = true" class="flex-auto w-1/4 h-1/4">
         <p>{{item}}</p>
+        <br>
       </div>
     </div>
+    <UModal v-model="isOpen">
+      <div class="flex flex-auto">
+        <div class="p-4">
+          <div class="h-72">
+
+            </div>
+        </div>
+      </div>
+    </UModal>
   </div>
 </template>
 
