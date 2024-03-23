a9877b549449389f333dbd0a4a3d3e7947d83a43
Ryan Bauroth <25bauroth@da.org>
2024-03-22 21:53:05 -0400

    test sync

--- pages/settings.vue
+++ pages/settings.vue
@@ -4,6 +4,16 @@ import SyncStatusVisualization from "~/components/SyncStatusVisualization.vue";
 import {ref} from "vue";
 import PouchDB from "pouchdb";
 import databases from "~/utils/databases";
+let syncDisable = ref(false)
+
+async function sync(){
+  syncDisable.value = true
+  await PouchDB.sync(databases.locals.scoutingData, databases.remotes.scoutingData)
+  await PouchDB.sync(databases.locals.basic, databases.remotes.basic)
+  await PouchDB.sync(databases.locals.attachments, databases.remotes.attachments)
+  await PouchDB.sync(databases.locals.teamInfo, databases.remotes.teamInfo)
+  syncDisable.value = false
+}
 
 const colorMode = useColorMode()
 const isDark = computed({
@@ -14,20 +24,13 @@ const isDark = computed({
     colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
   }
 })
-
-let syncDisable = ref(false)
-async function sync(){
-  syncDisable.value = true
-  await PouchDB.sync(databases.locals.scoutingData, databases.remotes.scoutingData)
-  await PouchDB.sync(databases.locals.basic, databases.remotes.basic)
-  await PouchDB.sync(databases.locals.attachments, databases.remotes.attachments)
-  await PouchDB.sync(databases.locals.teamInfo, databases.remotes.teamInfo)
-  syncDisable.value = false
-}
 </script>
 
 <template>
   <OuterComponents>
+    <UButton class="ml-3 mt-3" @click="sync" :disabled="syncDisable" :loading="syncDisable">
+      Sync Databases
+    </UButton>
     <div class="flex justify-center">
       <UCard class="max-w-xl mt-5 flex-grow">
         <UFormGroup class="inline-block inputDiv mb-2" label="Style">
