9a1e52db1fed2e4d4d19628f8ce20ab9d8c111a4
Ryan Bauroth <25bauroth@da.org>
2024-04-11 11:27:51 -0400

    changed eventkey around for funsies

--- composables/useEventKey.ts
+++ composables/useEventKey.ts
@@ -3,14 +3,17 @@ import {eventOptions} from "~/utils/eventOptions";
 export const useEventKey = () => {
     const eventKey = ref(eventOptions[0])
 
-    if (typeof window !== 'undefined') eventKey.value = localStorage.getItem('currentEvent') || eventOptions[0]
-
     function update(){
         if (typeof window !== 'undefined') eventKey.value = localStorage.getItem('currentEvent') || eventOptions[0]
+        else eventKey.value = eventOptions[0]
     }
 
-    onMounted(() => update)
-    onUnmounted(() => update)
+    onMounted(() => {
+        update()
+    })
+    onUnmounted(() => {
+        update()
+    })
 
     return eventKey
 }
