150ccafd7aca4b0282645bea1cf78a5ae3df56d4
Ryan Bauroth <25bauroth@da.org>
2024-06-20 15:48:20 -0400

    commented vuetify page

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,8 +1,13 @@
-import { createVuetify } from 'vuetify'
+/*
+A file config. vuetify is used for the matches page currently
+Ideally we will find a workaround with Nuxt shortly, however to allow the header capabilities we want for the moment this file is needed
+ */
 
-export default defineNuxtPlugin((app) => {
-    const vuetify = createVuetify({
-        // ... your configuration
-    })
-    app.vueApp.use(vuetify)
-})
+import { createVuetify } from 'vuetify';
+
+export default defineNuxtPlugin(app => {
+  const vuetify = createVuetify({
+    // ... your configuration
+  });
+  app.vueApp.use(vuetify);
+});
