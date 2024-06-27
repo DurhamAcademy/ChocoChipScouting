a3eba8b6f5e80dd1214b01393f802f9eada14072
Ryan Bauroth <25bauroth@da.org>
2024-06-26 21:41:46 -0400

    added some documentation

--- components/scouting-components/BooleanButton.vue
+++ components/scouting-components/BooleanButton.vue
@@ -1,39 +1,55 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-const emit = defineEmits(['update:modelValue'])
-const props = defineProps<{
-    modelValue: boolean
-    defaultValue: string
-    otherValue: string
-}>()
+import { computed } from 'vue';
 
+/** A component for a button that acts like a boolean value switch intended for the scouting page
+ *
+ * @modelValue the boolean being changed and returned
+ * @defaultValue the default label for the button (default is set when you input a model value)
+ * @otherValue the not-default label for the button
+ */
+const props = defineProps<{
+  modelValue: boolean;
+  defaultValue: string;
+  otherValue: string;
+}>();
+const emit = defineEmits(['update:modelValue']);
+
+/**
+ * Getter and setter for the modelValue
+ */
 const value = computed({
-    get() {
-        return props.modelValue
-    },
-    set(value) {
-        emit('update:modelValue', value)
-    },
-})
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    emit('update:modelValue', value);
+  },
+});
 
 function selected() {
-    value.value = !value.value
-    variantReference.value = bool ? 'outline' : 'solid'
-    labelReference.value = bool ? props.defaultValue : props.otherValue
-    bool = !bool
+  value.value = !value.value;
+  variantReference.value = bool ? 'outline' : 'solid';
+  labelReference.value = bool ? props.defaultValue : props.otherValue;
+  bool = !bool;
 }
 
 /**
  * Probably unnecessary
  */
-let bool = props.modelValue
+let bool = props.modelValue;
 
-let variantReference = ref(props.modelValue ? 'solid' : 'outline')
-let labelReference = ref(props.modelValue ? props.otherValue : props.defaultValue)
+let variantReference = ref(props.modelValue ? 'solid' : 'outline');
+let labelReference = ref(
+  props.modelValue ? props.otherValue : props.defaultValue,
+);
 </script>
 
 <template>
-    <UButton @click="selected()" :label="labelReference" :variant="variantReference"></UButton>
+  <UButton
+    @click="selected()"
+    :label="labelReference"
+    :variant="variantReference"
+  ></UButton>
 </template>
 
 <style scoped></style>
--- components/scouting-components/IncrementalButton.vue
+++ components/scouting-components/IncrementalButton.vue
@@ -1,32 +1,41 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-/*
-Model Value: the number selected
-Max Value: the maximum value possible
-(implied min value): must be above 0, can be changed in code below if need be
+import { computed } from 'vue';
+/**
+ A component for an incremental button intended for the scouting page
+ looks like:
+ - 0 + => - 1 + => - 2 + => etc
+ and returns the number in the middle
+
+@modelValue: the number being changed and returned as the input value
+@maxValue: the maximum value possible, defaults to infinity
+@minValue: the minimum value possible, defaults to 0
  */
 const props = defineProps<{
-    modelValue: number
-    maxValue?: number
-}>()
-const emit = defineEmits(['update:modelValue'])
+  modelValue: number;
+  maxValue?: number;
+  minValue?: number;
+}>();
+const emit = defineEmits(['update:modelValue']);
 
+/**
+ * Getter and setter for the modelValue
+ */
 const value = computed({
-    get() {
-        return props.modelValue
-    },
-    set(value) {
-        emit('update:modelValue', value)
-    },
-})
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    emit('update:modelValue', value);
+  },
+});
 
 /**
  * Function addToValue()
  * adds one to the value of the component
  */
 function addToValue() {
-    if (props.maxValue == undefined) value.value++
-    else if (value.value < props.maxValue) value.value++
+  if (props.maxValue == undefined) value.value++;
+  else if (value.value < props.maxValue) value.value++;
 }
 
 /**
@@ -34,16 +43,36 @@ function addToValue() {
  * subtracts one from the value of the component such that the value cannot go below zero
  */
 function subtractFromValue() {
-    if (value.value > 0) value.value--
+  let limitValue = props.minValue != undefined ? props.minValue : 0;
+  if (value.value > limitValue) value.value--;
 }
 </script>
 
 <template>
-    <UButtonGroup size="sm" orientation="horizontal" class="mb-2.5">
-        <UButton color="primary" variant="solid" @click="subtractFromValue()">-</UButton>
-        <UButton disabled color="primary" variant="solid" :label="value.toString()"></UButton>
-        <UButton color="primary" variant="solid" @click="addToValue()">+</UButton>
-    </UButtonGroup>
+  <UButtonGroup
+    size="sm"
+    orientation="horizontal"
+    class="mb-2.5"
+  >
+    <UButton
+      color="primary"
+      variant="solid"
+      @click="subtractFromValue()"
+      >-</UButton
+    >
+    <UButton
+      disabled
+      color="primary"
+      variant="solid"
+      :label="value.toString()"
+    ></UButton>
+    <UButton
+      color="primary"
+      variant="solid"
+      @click="addToValue()"
+      >+</UButton
+    >
+  </UButtonGroup>
 </template>
 
 <style scoped></style>
