a31f5bfc781e9dcea3b99647c5a4f8a13d81b358
Ryan Bauroth <25bauroth@da.org>
2024-11-22 15:02:35 -0500

    debug

--- start-production.sh
+++ start-production.sh
@@ -1,4 +1,5 @@
 mkdir /home/ubuntu/webapp
+echo "running start-production"
 cd /home/ubuntu/webapp
 cat /home/ubuntu/.CR_PAT.txt | sudo docker login ghcr.io -u da-robotics --password-stdin
 sudo docker compose pull
