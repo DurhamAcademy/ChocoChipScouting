a78e1d653d33d1b0acf90b17e75e3eea7566fcae
Preston Swigart <prestonlswigart@gmail.com>
2025-02-06 15:51:06 -0500

    this is being (slowly) worked on

--- utils/scoreMatch.ts
+++ utils/scoreMatch.ts
@@ -1,4 +1,5 @@
 export function scoreMatch(match: any){
+    console.dir(match)
     let autoScores = match.auto.coralL1 * 3 + match.auto.coralL2 * 4 + match.auto.coralL3 * 6 + match.auto.coralL4 * 7
     let teleopScores = match.teleop.coralL1 * 2 + match.teleop.coralL2 * 3 + match.teleop.coralL3 * 4 + match.teleop.coralL4 * 5 + match.teleop.processor * 6 + match.teleop.net * 4
     let endgameScores = 0
