c5b8ab9fb95b99fa94c2c5fcfc60b0f1250d5206
Ryan Bauroth <25bauroth@da.org>
2025-02-09 11:02:07 -0500

    server testing

--- modules/setupTeamInfoDatabase.ts
+++ modules/setupTeamInfoDatabase.ts
@@ -20,11 +20,11 @@ export default defineNuxtModule({
 
         // Try to read the existing file
         try {
-          // if (!existsSync(DATA_DIR)) {
-          //   await mkdir(DATA_DIR, { recursive: true });
-          // }
-          // const fileData = await readFile(TEAM_INFO_PATH, 'utf-8');
-          // previouslySavedEvents = JSON.parse(fileData).events || [];
+          if (!existsSync(DATA_DIR)) {
+            await mkdir(DATA_DIR, { recursive: true });
+          }
+          const fileData = await readFile(TEAM_INFO_PATH, 'utf-8');
+          previouslySavedEvents = JSON.parse(fileData).events || [];
         } catch (error) {
           console.log(
             'No existing eventTeamInfo.json found. Creating a new one.',
@@ -97,12 +97,12 @@ export default defineNuxtModule({
             // Merge new teams with existing ones
             const finalEvents = [...previouslySavedEvents, ...updatedEvents];
 
-            // // Save updated data to file
-            // await writeFile(
-            //   TEAM_INFO_PATH,
-            //   JSON.stringify({ events: finalEvents }, null, 2),
-            //   'utf-8',
-            // );
+            // Save updated data to file
+            await writeFile(
+              TEAM_INFO_PATH,
+              JSON.stringify({ events: finalEvents }, null, 2),
+              'utf-8',
+            );
             console.log(
               `Updated eventTeamInfo.json with ${finalEvents.length} new events.`,
             );
