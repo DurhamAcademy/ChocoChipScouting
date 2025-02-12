72edba3690af616af7311e07063fef35bb3debe2
Ryan Bauroth <25bauroth@da.org>
2025-02-12 08:18:36 -0500

    server testing

--- modules/setupTeamInfoDatabase.ts
+++ modules/setupTeamInfoDatabase.ts
@@ -1,6 +1,6 @@
 import { resolve } from 'path';
 import { writeFile, readFile, mkdir } from 'fs/promises';
-import { eventOptions } from '~/utils/eventOptions';
+import { eventOptions } from './utils/eventOptions';
 import { EventData, TeamInfo } from '~/utils/databases';
 import { existsSync } from 'node:fs';
 import { defineNuxtModule } from '@nuxt/kit'; // Adjust path as needed
