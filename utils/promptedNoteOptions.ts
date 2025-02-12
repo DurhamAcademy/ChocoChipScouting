6ae6e04309b03adb87db4c4418e130d5e6addbbc
Ryan Bauroth <25bauroth@da.org>
2025-02-12 16:18:30 -0500

    summaries addition

--- utils/promptedNoteOptions.ts
+++ utils/promptedNoteOptions.ts
@@ -9,6 +9,7 @@ export const promptedNoteOptions = [
       'What area(s) of weakness would you want to make sure this team avoids in the offensive strategy?',
       'Is there anything else that the quantitative data might not highlight about this team?',
     ],
+    summaries: ['Area of strength', 'Area(s) of weakness', 'Other notes'],
   },
   {
     name: 'Defense',
@@ -16,6 +17,11 @@ export const promptedNoteOptions = [
       "What seemed to be this team's defensive strategy? What area, team, or other factor were they focused on?",
       'Was their defensive strategy successful, or is there a different defensive strategy this team should employ to make more of an impact?',
       'What other factors contributed to your rating?',
+    ],
+    summaries: [
+      'Defensive strategy',
+      'Defensive success/failure',
+      'Other notes',
     ],
   },
   {
@@ -25,6 +31,7 @@ export const promptedNoteOptions = [
       'How aggressive was this driver? Did they inflict a lot of penalties on their team? If so, could you tell what for?',
       'What other factors contributed to your rating?',
     ],
+    summaries: ['Game sense', 'Aggressiveness', 'Other notes'],
   },
   {
     name: 'Robustness',
@@ -32,5 +39,6 @@ export const promptedNoteOptions = [
       "What happened on the field that led you to select this category? Did this team's robot break, or survive a big hit?",
       'Do you think this will be a consistent issue/strength, or was it a one time thing?',
     ],
+    summaries: ['Point of strength/failure', 'Reoccurring issue?'],
   },
 ];
