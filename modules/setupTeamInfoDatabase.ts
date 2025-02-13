3fd72c20772ea34469963ee0f3077f2f9d23c26b
Ryan Bauroth <25bauroth@da.org>
2025-02-13 10:29:58 -0500

    server testing

--- modules/setupTeamInfoDatabase.ts
+++ modules/setupTeamInfoDatabase.ts
@@ -39,7 +39,7 @@ export default defineNuxtModule({
 
         let updatedEvents: EventData[] = [];
 
-        for (let event of ['2025ncmec']) {
+        for (let event of eventOptions) {
           console.log(event);
           // if (TBA_KEY) {
           //   let urlNoNum: string = 'https://www.thebluealliance.com/api/v3/';
