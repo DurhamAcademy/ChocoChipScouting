772f30279e777ea2e8a2acfc31261713271a348b
Preston <112106776+PrestonSwigart@users.noreply.github.com>
2025-02-14 15:47:27 -0500

    Update scoreMatch.ts

--- utils/scoreMatch.ts
+++ utils/scoreMatch.ts
@@ -1,11 +1,5 @@
 export function scoreMatch(match: any){
-    let autoScores = match.auto.coralL1 * 3 + match.auto.coralL2 * 4 + match.auto.coralL3 * 6 + match.auto.coralL4 * 7
-    let teleopScores = match.teleop.coralL1 * 2 + match.teleop.coralL2 * 3 + match.teleop.coralL3 * 4 + match.teleop.coralL4 * 5 + match.teleop.processor * 6 + match.teleop.net * 4
-    let endgameScores = 0
-    if(match.endgame.endgame.includes("Parked")) endgameScores += 2
-    if(match.endgame.endgame.includes("Shallow")) endgameScores += 6
-    if(match.endgame.endgame.includes("Deep")) endgameScores += 12
-    return autoScores + teleopScores + endgameScores
+    return scoreMatchAuto(match) + scoreMatchTeleop(match) + scoreMatchEndgame(match);
 }
 
 export function scoreMatchAuto(match: any){
