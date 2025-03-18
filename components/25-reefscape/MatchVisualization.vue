8641e41b23edd6bc39ad2e42b40221f910538fd5
Preston Swigart <prestonlswigart@gmail.com>
2025-03-18 14:29:27 -0400

    this needs to be tested on server

--- components/25-reefscape/MatchVisualization.vue
+++ components/25-reefscape/MatchVisualization.vue
@@ -117,7 +117,7 @@ let promptedNotesDetailedOptions = promptedNoteOptions.map(
       <div class="flex-auto whitespace-normal max-h-72 max-w-1/2 w-1/2 ml-4">
         <p class="font-extrabold text-sm dark:text-white">Average Penalties: </p>
          <UBadge
-          color="sky"
+          color="red"
           variant="subtle"
           v-if="props.rowData.penaltyScore > 0"
           class="mr-1.5 mt-2">
