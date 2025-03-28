da482df3fca3e4c2c427b8b63e72571125f68b76
Preston Swigart <prestonlswigart@gmail.com>
2025-03-28 13:15:20 -0400

    man i dont even know

--- pages/teams/[id].vue
+++ pages/teams/[id].vue
@@ -135,7 +135,7 @@ function setup() {
     if (typeof key == 'string') key = parseInt(key);
     let filteredValue: (ScoutingData & IdMeta)[] = [];
     for (let match of value) {
-      if (match.event === useEventKey().value) {
+      if (match.event === localStorage.getItem('currentEvent')) {
         if (!teamOptions.value.includes(key.toString())) {
           teamOptions.value.push(key.toString());
         }
